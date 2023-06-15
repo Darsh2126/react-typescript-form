45077780479cea32e46fc4772bc7546435c9989d
Darsh <darsh2126gajdhar@gmail.com>
2023-06-16 00:19:34 +0530

    Configure ESLINT and Prettier in project

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
+  rules: {},
+  // react/function-component-definition: [
+  //   2,
+  //   {
+  //     namedComponents: 'arrow-function',
+  //     unnamedComponents: 'arrow-function',
+  //   },
+  // ],
+};
